6851816180e3cc38b3d80c8d8237a86a0dc8787c
DrRawley <rawley.greene@gmail.com>
2024-05-07 12:45:31 -0400

    pandas test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,18 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 
-# center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)
+# # center on Liberty Bell, add marker
+# m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+# folium.Marker(
+#     [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+# ).add_to(m)
 
-# call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
+# # call to render Folium map in Streamlit
+# st_data = st_folium(m, width=725)
 
+df = pd.read_csv('europe.csv')
 
+print(df)
 
 
 
